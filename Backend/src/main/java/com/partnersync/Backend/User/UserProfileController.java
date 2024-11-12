302aaaf500c64c135006ce5d4ef0f525ff193abb
Chinurum-Omenuko <omen0003@algonquinlive.com>
2024-11-12 12:10:15 -0500

    adds new userprofile controller

---
+++ Backend/src/main/java/com/partnersync/Backend/User/UserProfileController.java
@@ -0,0 +1,28 @@
+package com.partnersync.Backend.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user-profile")
+public class UserProfileController {
+
+    @GetMapping
+    public ResponseEntity<T> getUserProfile(){
+
+    }
+
+    @PostMapping
+    public ResponseEntity<T> createOrUpdateUserProfile(){
+
+    }
+
+    @GetMapping("/role")
+    public ResponseEntity<String> getUserRole(){
+
+    }
+
+}
