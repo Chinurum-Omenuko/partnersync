1ebebf9db1d48eb3a5d107199cf373f12a478bca
Chinurum-Omenuko <omen0003@algonquinlive.com>
2024-11-21 02:49:56 -0500

    adds metadata to claims to get user details

--- frontend/src/routes/+layout.server.ts
+++ frontend/src/routes/+layout.server.ts
@@ -2,6 +2,7 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
   return {
-    user: locals.session?.userId
+    user: locals.session?.userId,
+    claims: locals.session?.claims
   };
 };
--- frontend/src/routes/profile/+page.svelte
+++ frontend/src/routes/profile/+page.svelte
@@ -3,10 +3,12 @@
 	import SignOutButton from "clerk-sveltekit/client/SignOutButton.svelte";
 
     let username = '';
+    
     export let data
+    console.log(data.claims)
 </script>
 
 
-<h1>Welcome {username} {data.user}</h1>
+<h1>Welcome {username} {data.claims.first_name}</h1>
 <SignedIn let:user />
 <SignOutButton />
